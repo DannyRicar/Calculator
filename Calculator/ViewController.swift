f634adfbd0fd5324fe9becca565cc093f39a1b0f
DannyRicar <daniel.ricar@educanet.cz>
2019-10-22 14:05:58 +0200

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Dan Říčař on 22/10/2019.
+//  Copyright © 2019 Dan Říčař. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
